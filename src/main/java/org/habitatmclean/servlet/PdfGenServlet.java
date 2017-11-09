52b511198f6fb1e09b11d09f77e43b9ea2662ee0
Abe Ramseyer <aramsey@ilstu.edu>
2017-11-09 10:25:03 -0600

    pdf generation using temporary directories, file cleanup, and loading from a properties file

--- src/main/java/org/habitatmclean/servlet/PdfGenServlet.java
+++ src/main/java/org/habitatmclean/servlet/PdfGenServlet.java
@@ -5,40 +5,83 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Properties;
 
 @WebServlet(name = "PdfGenServlet", value="/pdfgen")
 public class PdfGenServlet extends HttpServlet {
+    private static String installDirectory;
+    private static String tempDirectory = System.getProperty("java.io.tmpdir") + "\\";
+
+    @Override
+    public void init() throws ServletException {
+        // read PdfGeneration.properties for install directory of wkhtmltopdf
+        Properties properties = new Properties();
+        InputStream input = null;
+        try {
+            input = getClass().getClassLoader().getResourceAsStream("PdfGeneration.properties");
+            properties.load(input);
+            installDirectory = (String) properties.get("wkhtmltopdf_install_directory");
+            System.out.println(installDirectory);
+        } catch (IOException e) {
+            System.err.println("pdf generation properties file wasn't located");
+        } finally {
+            if(input != null){
+                try { input.close(); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+        }
+    }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String[] cmdArr = {"C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf", "localhost:8085/dbservlet", "table2.pdf"};
+        long ts = System.currentTimeMillis();
+        String type = request.getParameter("page");
+        String option = "";
+        String[] cmdArr = { };
+        if(type.equals("log")) {
+            option = request.getParameter("options");
+            String url = "http://localhost:8085/" + type + ".html?type=" + option; // TODO change this whenever logs are made
+
+            cmdArr = new String[]{installDirectory + "/bin/wkhtmltopdf.exe", "--viewport-size", "1280x1024", "-O", "landscape", url, tempDirectory + ts + ".pdf"};
+        } else {
+            cmdArr = new String[]{installDirectory + "bin/wkhtmltopdf.exe", "--viewport-size", "1280x1024", "-O", "landscape", "http://localhost:8085/" + type + ".html", tempDirectory + ts + ".pdf"};
+        }
         Process p = new ProcessBuilder(cmdArr).start();
         try {
+            System.out.println(Arrays.toString(cmdArr));
             p.waitFor();
         } catch (InterruptedException e) {
             System.err.println(e.getStackTrace().toString());
         }
+
         OutputStream out = response.getOutputStream();
         FileInputStream in = null;
         try {
-            in = new FileInputStream("C:\\Program Files\\wkhtmltopdf\\bin\\table2.pdf");
+            in = new FileInputStream(tempDirectory + ts + ".pdf");
         } catch (FileNotFoundException e) {
-            System.out.println("file not found");
+            System.err.println("file not found, ensure app has write permissions to the directory:\n" + tempDirectory);
             out.write("file wasn't generated".getBytes());
         }
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = in.read(buffer)) > 0){
-            out.write(buffer, 0, length);
+
+        // file is open for sending
+        if(in != null) {
+            byte[] buffer = new byte[4096];
+            int length;
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+            in.close();
+            out.flush();
+            try {
+                File f = new File(tempDirectory + ts + ".pdf");
+                if(!f.delete()) System.err.println("error cleaning up files in:\n" + tempDirectory + ts + ".pdf");
+            } catch (Exception e) {
+            }
         }
-        in.close();
-        out.flush();
     }
 }
