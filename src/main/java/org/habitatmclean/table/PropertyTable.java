80d4a6758ac95e7da06990c3508c828b8cf387b8
gstrain <Kamino2933>
2017-11-10 17:21:39 -0600

    Log buttons added dynamically with flags, no log table yet

--- src/main/java/org/habitatmclean/table/PropertyTable.java
+++ src/main/java/org/habitatmclean/table/PropertyTable.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class PropertyTable<P> extends Table {
     public PropertyTable() {
-        super(new String[]{"property_no", "owner", "property status"},new PropertyModal() ); // adjust this to determine table columns
+        super(new String[]{"property_no", "owner", "property status"},new PropertyModal(), true); // adjust this to determine table columns
     }
     @Override
     public void addRow(GenericEntity entity) {
