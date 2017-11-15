86b518486eac7ed2a1cd6538f517f3a1be44c897
gstrain <Kamino2933>
2017-11-15 00:41:10 -0600

    Page table search. Currently searches the whole table.

--- web/scripts/search.js
+++ web/scripts/search.js
@@ -1,21 +1,19 @@
-function myFunction() {
+function searchTable() {
     // Declare variables
-    var input, filter, table, tr, i, j, row, col;
+    var input, filter, table, i, j, row, col;
     input = document.getElementById("search");
+    // input = it.id;
     filter = input.value.toUpperCase();
     table = document.getElementById("table");
-    tr = table.getElementsByTagName("tr");
 
-    for (i = 0, row; row = table.rows[i]; i++) {
+    for (i = 1, row; row = table.rows[i]; i++) {
         for (j = 0, col; col = row.cells[j]; j++) {
-            td = tr[i].getElementsByTagName("td")[i];
-            console.log(td);
-            if (td) {
-              if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
+            if (col) {
+              if (col.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                row.style.display = "";
                 break;
               } else {
-                tr[i].style.display = "none";
+                row.style.display = "none";
               }
             }
         }
