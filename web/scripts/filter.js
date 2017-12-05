772ebd409d51e96155f45a92f68a1e788381f036
gstrain <Kamino2933>
2017-12-05 00:22:12 -0600

    Removed debug log print

--- web/scripts/filter.js
+++ web/scripts/filter.js
@@ -14,10 +14,6 @@ column = function(p) {
                     if (row[i].style.display != "none") {
                         col = row[i].getElementsByTagName("td")[j];
                         if (col) {
-                            console.log(j);
-                            console.log(cParam);
-                            console.log(col.innerText.toUpperCase());
-                            console.log(col.innerText.toUpperCase().indexOf(cParam) + "\n\n\n\n");
                             if (col.innerText.toUpperCase().indexOf(cParam) > -1) {
                                 row[i].style.display = "";
                             } else {
