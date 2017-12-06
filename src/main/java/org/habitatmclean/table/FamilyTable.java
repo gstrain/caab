864bb4846ed6bdcf1545772a3dfbfb9cb94e178b
Fehr, Andrew <ajfehr@ilstu.edu>
2017-12-06 09:41:38 -0600

    family report removal since its broke and no time to fix

--- src/main/java/org/habitatmclean/table/FamilyTable.java
+++ src/main/java/org/habitatmclean/table/FamilyTable.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 public class FamilyTable extends Table {
     public FamilyTable() {
-        super(new String[]{"class", "milestone", "equity hours", "income"}, new FamilyModal(), true, true); // adjust this to determine table columns
+        super(new String[]{"class", "milestone", "equity hours", "income"}, new FamilyModal(), true, false); // adjust this to determine table columns
     }
     public void addRow(GenericEntity entity) {
         Family family = (Family) entity;
